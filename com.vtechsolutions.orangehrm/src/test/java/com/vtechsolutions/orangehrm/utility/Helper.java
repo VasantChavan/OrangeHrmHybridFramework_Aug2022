52b81dc06aba516dd0046c2d8e71c6ea42fb6b96
Vasant <vasant.chavan223@gmail.com>
2022-08-29 20:40:11 +0530

    Modified Helper.java file

--- com.vtechsolutions.orangehrm/src/test/java/com/vtechsolutions/orangehrm/utility/Helper.java
+++ com.vtechsolutions.orangehrm/src/test/java/com/vtechsolutions/orangehrm/utility/Helper.java
@@ -1,1 +1,11 @@
-This is a class file for all helper utility
+
+
+//This is a class file for all helper utility
+//This is a class file for all helper utility
+
+//This is a class file for all helper utility
+
+//This is a class file for all helper utility
+
+//This is a class file for all helper utility
+
